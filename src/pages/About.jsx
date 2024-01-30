76237e24bcb70fe91ea4bb4a2c32c43c3414faed
Shaid Islam <shaid85@gmail.com>
2024-01-30 18:07:59 +0600

    Home page content

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,9 +3,58 @@ import { Container } from '../components'
 
 function About() {
   return (
-    <Container>
-      About
-    </Container>
+    <div className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
+      <img
+        src="https://images.pexels.com/photos/2653362/pexels-photo-2653362.jpeg"
+        alt=""
+        className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
+      />
+      <div className="absolute inset-0 -z-10 h-full w-full bg-black opacity-80"></div>
+      <div
+        className="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl"
+        aria-hidden="true"
+      >
+        <div
+          className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#531930] to-[#272075] opacity-30"
+          style={{
+            clipPath:
+              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+          }}
+        />
+      </div>
+      <div
+        className="absolute -top-52 left-1/2 -z-10 -translate-x-1/2 transform-gpu blur-3xl sm:top-[-28rem] sm:ml-16 sm:translate-x-0 sm:transform-gpu"
+        aria-hidden="true"
+      >
+        <div
+          className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#722d4a] to-[#302d6d] opacity-20"
+          style={{
+            clipPath:
+              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+          }}
+        />
+      </div>
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:mx-0">
+          <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">About me</h2>
+          <p className="mt-6 text-lg leading-8 text-gray-300">
+          I am a full-stack Web developer. I have over 10 years of experience in this field. 
+          I'm expert in creating and customizing WordPress websites, including themes, plugins, 
+          and custom post types. I'm expert in both Front-end Development and Back-end development. 
+          I also have advanced knowledge of ACF Pro, Elementor Pro, Woocommerce, HTML5, CSS3, Bootstap.<br/><br/>
+          My Offered WordPress services:<br/>
+- PSD to WordPress( Elementor/ACF Pro)<br/>
+- Responsive websites with Elementor.<br/>
+- Custom Post Type & Advanced Custom Fields Pro integration.<br/>
+- E-commerce website with WooCommerce.<br/>
+- WordPress site maintenance, migration/transfer.<br/>
+- Fix any kind of CSS bug / issue.<br/>
+          </p>
+
+        </div>
+
+      </div>
+    </div>
   )
 }
 
